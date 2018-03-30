616b98f77f7e4d52cb01c8451bdf2b5544d4d381
Jeff Vega <jeff@intellibliss.com>
2018-03-30 14:43:10 -0700

    style fixed, and html fixed need more work

---
+++ Public/Scripts/store.js
@@ -0,0 +1,16 @@
+const STORE = {
+  bookmarks:[
+  {
+    id:cuid(),
+    title:'google',
+    url:"https://google.com",
+    description: 'help',
+    rating:null
+  },
+ 
+],
+filterMySearch:false,
+rating:null,
+adding:false,
+hide:false
+};
